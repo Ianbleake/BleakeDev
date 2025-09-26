d78b283d5e35ffee68b27d33bc988a842d2234bf
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-26 16:07:49 -0600

    feat: add 404 page

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Button from "./ui/button";
+import Link from "next/link";
 
 export default function Header(): React.ReactElement {
   return (
     <header className="fixed h-16 w-full bg-gray-800 flex items-center justify-between px-8">
-      <h1 className="text-2xl font-semibold text-white">Bleake<span className="text-emerald-600">Dev</span></h1>
+      <Link href={"/"} >
+        <h1 className="text-2xl font-semibold text-white">Bleake<span className="text-emerald-600">Dev</span></h1>
+      </Link>
       <nav className="none md:flex items-center gap-4">
         <a href="#about" className="text-lg text-stone-300 hover:text-emerald-400 transition-colors font-medium">Sobre mi</a>
         <a href="#work" className="text-lg text-stone-300 hover:text-emerald-400 transition-colors font-medium">Proyectos</a>
