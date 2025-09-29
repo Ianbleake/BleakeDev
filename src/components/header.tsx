b31f0ecd90b78fb2647ef61a706fb16d58288916
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-29 13:36:09 -0600

    feat: add contact page header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,35 @@
+'use client'
+
 import React from "react";
 import Button from "./ui/button";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { IoMdArrowRoundBack } from "react-icons/io";
+import { twTheme } from "@/utils/ThemeColors";
+import { SiGithub } from "react-icons/si";
 
 export default function Header(): React.ReactElement {
 
-  
+  const pathname = usePathname();
+
+  if (pathname === "/contact") return (
+    <header className="fixed h-16 w-full bg-gray-800 flex items-center justify-between px-8">
+
+      <Link href={"/"} className="cursor-pointer">
+        <IoMdArrowRoundBack size={30} color={twTheme.colors.emerald[600]} />
+      </Link>
+
+      <Link href={"/"} >
+        <h1 className="text-2xl font-semibold text-white">Bleake<span className="text-emerald-600">Dev</span></h1>
+      </Link>
+
+      <a href="#" target="_blank">
+        <SiGithub size={30} color={twTheme.colors.emerald[600]} />
+      </a>
+
+    </header>
+  );
+
   return (
     <header className="fixed h-16 w-full bg-gray-800 flex items-center justify-between px-8">
       <Link href={"/"} >
