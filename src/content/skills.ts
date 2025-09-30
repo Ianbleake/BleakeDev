478d5b46e722f2edf4ab42b7db8a4bb85808a5e7
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-30 10:47:10 -0600

    fix: update skills

--- src/content/skills.ts
+++ src/content/skills.ts
@@ -1,8 +1,31 @@
-import { FaCode, FaPalette } from "react-icons/fa6";
+import { FaCode, FaMobileAlt, FaServer } from "react-icons/fa";
+import { SiShopify } from "react-icons/si";
 import { GoZap } from "react-icons/go";
 
-export   const skills = [
-  { name: "Frontend", icon: FaCode, technologies: ["React", "TypeScript", "Next.js", "Tailwind"] },
-  { name: "Design", icon: FaPalette, technologies: ["Figma", "UI/UX", "Responsive", "Animations"] },
-  { name: "Tools", icon: GoZap, technologies: ["Git", "Webpack", "Vite", "Testing"] }
+export const skills = [
+  { 
+    name: "Frontend", 
+    icon: FaCode, 
+    technologies: ["React", "TypeScript", "Next.js", "Tailwind", "Zustand","React Query"] 
+  },
+  { 
+    name: "Mobile", 
+    icon: FaMobileAlt, 
+    technologies: ["React Native", "Expo", "Navigation", "State Management"] 
+  },
+  { 
+    name: "E-commerce", 
+    icon: SiShopify, 
+    technologies: ["Shopify","Mercado Libre","Tienda Nube", "Liquid", "SEO", "Custom Themes"] 
+  },
+  { 
+    name: "Backend & Databases", 
+    icon: FaServer, 
+    technologies: ["Node.js", "Express", "Supabase", "Firebase", "MongoDB"] 
+  },
+  { 
+    name: "Tools", 
+    icon: GoZap, 
+    technologies: ["Git", "CI/CD", "Testing", "Postman"] 
+  }
 ];
