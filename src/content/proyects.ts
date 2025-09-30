b977999d1e2f4a7dd85b8df57cfcdd917be38b0b
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-09-30 12:34:31 -0600

    feat: add savy assets

--- src/content/proyects.ts
+++ src/content/proyects.ts
@@ -1,5 +1,6 @@
 export const projects = [
   {
+    id: 1,
     title: "Link Me Up",
     description: "Link Me Up is a PWA that allows bulk importing of contacts from Excel or CSV files, with the ability to review and edit the data before saving them to the device.",
     tech: ["React", "Vite", "TailwindCSS", "Zustand","TypeScript"],
@@ -9,13 +10,13 @@ export const projects = [
     image: "/images/linkmeup.png"
   },
   {
+    id: 2,
     title: "Savy - Budget App",
-    description: "Ongoing project focused on React Native + Supabase, structured by domain with clean service layers and React Query integration.",
+    description: "Savy is a mobile app designed to help users manage their personal finances by tracking income, expenses, and budgets in an intuitive way, without the complexity of traditional financial software.",
     tech: ["React Native", "Supabase", "React Query", "Zustand"],
     status: "In Progress",
     isExternal: false,
     link: "#",
-    image: "/projects/savy.png"
+    image: "/images/savySS1.png"
   }
-  
 ];
