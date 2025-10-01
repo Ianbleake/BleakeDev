aee62d5f994301d03c68b8dc073f9c308573664d
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-01 16:59:40 -0600

    fix: buttons padding

--- src/components/home/Introduction/components/actionButtons.tsx
+++ src/components/home/Introduction/components/actionButtons.tsx
@@ -17,7 +17,7 @@ export default function ActionButtons(): React.ReactElement {
 
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
-      <Button label="Explore My Projects" isLink={true} route="/proyects" />
+      <Button label="Explore My Projects" isLink={true} route="/proyects"className="px-8 py-3"/>
       <Button 
         onClick={handleDownloadCV}
         className="border-stone-600 text-stone-300 hover:text-white hover:bg-emerald-500/30 px-8 py-3 bg-transparent cursor-pointer flex items-center gap-4"
