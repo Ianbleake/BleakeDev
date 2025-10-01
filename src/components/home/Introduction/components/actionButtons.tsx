d2c8522d36055f553863a2ddaa4f0a83154a93bc
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-01 16:56:05 -0600

    feat: add download cv

--- src/components/home/Introduction/components/actionButtons.tsx
+++ src/components/home/Introduction/components/actionButtons.tsx
@@ -1,11 +1,30 @@
+"use client";
+
 import React from "react";
 import Button from "@/components/ui/button";
+import { IoCloudDownloadOutline } from "react-icons/io5";
 
 export default function ActionButtons(): React.ReactElement {
+
+  const handleDownloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/downloads/cv.pdf"; // archivo dentro de /public/downloads/
+    link.download = "IvanRangelCV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="flex flex-col sm:flex-row items-center justify-center md:justify-start gap-4">
       <Button label="Explore My Projects" isLink={true} route="/proyects" />
-      <Button label="Let’s Work Together" isLink={true} route="/contact" variant="secondary" />
+      <Button 
+        onClick={handleDownloadCV}
+        className="border-stone-600 text-stone-300 hover:text-white hover:bg-emerald-500/30 px-8 py-3 bg-transparent cursor-pointer flex items-center gap-4"
+      >
+        Download CV
+        <IoCloudDownloadOutline size={25} className="mr-2" />
+      </Button>
     </div>
   );
 }
