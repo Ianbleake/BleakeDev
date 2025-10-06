77272cab22b887fce14508ec6ecda223ecbe5907
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-06 16:01:26 -0600

    feat: add middleware and supabase

---
+++ src/middleware.ts
@@ -0,0 +1,51 @@
+import { NextResponse, type NextRequest } from "next/server";
+import { createServerClient } from "@supabase/ssr";
+
+export async function middleware(req: NextRequest) {
+  
+  const res = NextResponse.next({ request: req });
+
+  
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return req.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            res.cookies.set(name, value, options);
+          });
+        },
+      },
+    }
+  );
+
+  
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  const isAdminRoute = req.nextUrl.pathname.startsWith("/admin");
+  const isLoginRoute = req.nextUrl.pathname.startsWith("/admin/login");
+
+  if (isAdminRoute && !isLoginRoute && !session) {
+    const loginUrl = new URL("/admin/login", req.url);
+    loginUrl.searchParams.set("redirectedFrom", req.nextUrl.pathname);
+    return NextResponse.redirect(loginUrl);
+  }
+
+  if (isLoginRoute && session) {
+    const dashboardUrl = new URL("/admin", req.url);
+    return NextResponse.redirect(dashboardUrl);
+  }
+
+  return res;
+}
+
+// Configuramos las rutas que intercepta
+export const config = {
+  matcher: ["/admin/:path*"],
+};
