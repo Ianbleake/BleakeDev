9752b5615599876b052e2be07a2d5bee2e7c9caa
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-06 10:43:26 -0600

    feat: add admin page

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import Page from "@/components/ui/page";
+import React from "react";
+
+export default function AdminPage(): React.ReactElement {
+  return (
+    <Page>
+      AdminPage
+    </Page>
+  );
+}
