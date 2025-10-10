e4abac5828b057d2f53a71c3dae1f9f65f765e57
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-10 10:47:18 -0600

    fix: add fetchprofile to singin service and restructure the login mutation to simplify

--- src/hooks/auth/useLoginMutation.ts
+++ src/hooks/auth/useLoginMutation.ts
@@ -3,35 +3,30 @@ import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import { useAuthStore } from "@/storage/authStore";
-import { useProfileQuery } from "../profile/useProfileQuery";
 import { AppErrorShape } from "@/utils/errorHandler";
-import { useEffect } from "react";
+
 
 export function useLoginMutation() {
+
   const router = useRouter();
   const setAuth = useAuthStore((state) => state.setAuth);
-
-  const loginMutation = useMutation({
+  
+  return useMutation({
     mutationKey: ["login"],
     mutationFn: ({ email, password }: { email: string; password: string }) =>
       signIn(email, password),
-    onSuccess: () => {
-      toast.success("Inicio de sesión exitoso");
-      router.push("/admin");
+    onSuccess: async (data) => {
+    
+      setAuth(data.user, data.session, data.profile || null );
+      
+      toast.success(`Inicio de sesion exitoso, Bienvenido ${data.profile?.username || ''}.`);
+      
+      router.push('/admin');
       router.refresh();
+
     },
     onError: (error: AppErrorShape) => {
       toast.error(error.message);
-    },
-  });
-
-  const { data: profile } = useProfileQuery(loginMutation.data?.user.id || "");
-
-  useEffect(() => {
-    if (profile && loginMutation.data) {
-      setAuth(loginMutation.data.user, loginMutation.data.session, profile);
     }
-  }, [profile, loginMutation.data, setAuth]);
-
-  return loginMutation;
+  });
 }
--- src/services/auth/singIn.ts
+++ src/services/auth/singIn.ts
@@ -1,12 +1,49 @@
 import { supabaseBrowser } from "@/supabase/client";
 import { handleError } from "@/utils/errorHandler";
+import { fetchUserProfile } from "../profile/fetchUserProfile";
+import { Session, User } from "@supabase/supabase-js";
 
-export async function signIn(email: string, password: string) {
+type singInResponse =  {   
+  profile: UserProfile | undefined;
+  user: User;
+  session: Session;
+}
+
+export async function signIn(email: string, password: string): Promise<singInResponse> {
+
   try{
+    
+    console.log("Iniciando login")
+    let profile = undefined;
 
     const { data, error } = await supabaseBrowser.auth.signInWithPassword({ email, password, });
+
     if (error) handleError(error,"signIn");
-    return data;
+
+    if(data){
+      console.log("Login ✅")
+      console.log("Obteniendo profile")
+      profile = await fetchUserProfile(data.user.id) as UserProfile;
+      if(profile){
+        console.log("Profile ✅")
+      }
+    }
+
+    console.log("Auth data:", data)
+    console.log("Cleaning data")
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { weakPassword, ...cleanData } = data;
+
+    console.log("Data cleaned ✅:",cleanData)
+    console.log("Adding profile")
+    const userInfo = {
+      ...cleanData,
+      profile,
+    };
+
+    console.log("Profile added ✅", userInfo)
+
+    return userInfo;
 
   } 
   
