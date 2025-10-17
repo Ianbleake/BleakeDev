f2264d5cff5af76b999849c4558fae3112bbbd6c
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-10-17 10:07:38 -0600

    fix: transform the achievement card on achievement card trigger to the edit sheet form

--- src/app/(admin)/admin/education/[id]/GradeAchivements/AchievementCard/index.tsx
+++ src/app/(admin)/admin/education/[id]/GradeAchivements/AchievementCard/index.tsx
@@ -1,11 +1,9 @@
 
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
 import React, { useState } from "react";
-import AchievementEditTrigger from "./components/achievementEditTrigger";
+import AchievementEditTriggerCard from "./components/achievementEditTriggerCard";
 import AchievementEditHeader from "./components/achievementEditHeader";
-import { GoTrophy } from "react-icons/go";
-import { twTheme } from "@/utils/ThemeColors";
-import { Card } from "@/components/ui/card";
+
 
 
 type AchievementCardProps = {
@@ -21,13 +19,8 @@ export default function AchievementCard({
   return (
     <Sheet open={open} onOpenChange={setOpen} >
       
-      <SheetTrigger asChild>
-        <Card className="flex flex-row gap-4 items-center px-4 hover:bg-green-50 cursor-pointer">
-          <GoTrophy size={30} color={twTheme.colors.emerald[600]} />
-          <p className="text-gray-400 text-sm font-normal">{achievement.description}</p>
-        </Card>
-      </SheetTrigger>
 
+      <AchievementEditTriggerCard achievement={achievement} />
 
       <SheetContent>
 
