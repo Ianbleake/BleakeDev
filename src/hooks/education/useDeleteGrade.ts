14d7e2dd0fb4ce9d017edec4b57e96a1c4bb2c5d
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-11-05 16:06:30 -0600

    chore: add redirect on delete grade hook

--- src/hooks/education/useDeleteGrade.ts
+++ src/hooks/education/useDeleteGrade.ts
@@ -2,10 +2,14 @@ import deleteCertification from "@/services/education/deleteCertification";
 import { deleteDegree } from "@/services/education/deleteDegree";
 import { AppErrorShape } from "@/utils/errorHandler";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 
 export default function useDeleteGrade() {
+
+  const router = useRouter();
+  
   const deleteGradeMutation = useMutation({
     mutationKey: ["deleteGrade"],
     mutationFn: async ({ gradeId, type }: { gradeId: string; type: string }) => {
@@ -16,6 +20,8 @@ export default function useDeleteGrade() {
     },
     onSuccess: () => {
       toast.success("Grade deleted successfully");
+      router.push("/admin/education"); 
+      router.refresh();
     },
     onError: (error: AppErrorShape) => {
       toast.error(error.message);
