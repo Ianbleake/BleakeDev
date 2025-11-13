ffa97e42812c17f7358623ca69d879e72b6930ea
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-11-13 17:35:09 -0600

    fix: updateProfile service unused vars and categoryCard unused category property

--- src/components/admin/categories/CategoriesGrid/components/categoryCard.tsx
+++ src/components/admin/categories/CategoriesGrid/components/categoryCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { Copy } from "lucide-react";
 import { twTheme } from "@/utils/ThemeColors";
 
@@ -15,17 +14,7 @@ export default function CategoryCard({
       <div
         className="h-10 w-10 flex items-center justify-center rounded-md border border-gray-200 shadow-md bg-green-50"
       >
-        {category.iconurl ? (
-          <Image
-            src={category.iconurl}
-            alt={category.name}
-            width={24}
-            height={24}
-            className="object-contain"
-          />
-        ) : (
-          <Copy size={20} color={twTheme.colors.emerald[600]} />
-        )}
+          <Copy size={20} color={twTheme.colors.emerald[600]} /> 
       </div>
 
       <p className="font-medium text-sm">{category.name}</p>
--- src/services/profile/updateProfile.ts
+++ src/services/profile/updateProfile.ts
@@ -3,7 +3,17 @@ import { handleError } from '@/utils/errorHandler';
 
 export async function updateProfile(updates: UserProfile): Promise<UserProfile> {
 
-  const { name, initials, ...updatedProfile } = updates;
+  const updatedProfile: Partial<UserProfile> = {
+    id: updates.id,
+    first_name: updates.first_name,
+    last_name: updates.last_name,
+    username: updates.username,
+    email: updates.email,
+    avatarurl: updates.avatarurl,
+    created_at: updates.created_at,
+    color: updates.color,
+    banner: updates.banner,
+  };
 
   try {
     const { data, error } = await supabaseBrowser
