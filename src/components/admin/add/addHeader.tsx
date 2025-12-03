205334ef2f99028c16b90519e3474b55cc85e47d
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-12-03 13:16:16 -0600

    fix: add header

--- src/components/admin/add/addHeader.tsx
+++ src/components/admin/add/addHeader.tsx
@@ -1,8 +1,9 @@
 import { Separator } from "@/components/ui/separator";
 import { twTheme } from "@/utils/ThemeColors";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { IconType } from "react-icons";
-import { IoSchoolOutline } from "react-icons/io5";
+import { IoChevronBack, IoSchoolOutline } from "react-icons/io5";
 import { PiCertificate } from "react-icons/pi";
 
 
@@ -19,13 +20,23 @@ export default function AddHeader({
   config,
 }:AddGradeHeaderProps ): React.ReactElement {
 
+  const router = useRouter();
+
   const Icon = config.icon;
 
   return (
     <div className="border-b border-gray-200 pb-4 flex flex-row items-center justify-start gap-6">
 
-        <div className="border border-gray-200 p-3 rounded-md shadow-sm bg-green-50 ">
-          <Icon size={30} color={twTheme.colors.emerald[700]} />
+        <div onClick={router.back} className="flex flex-row items-center justify-start gap-2 cursor-pointer">
+
+          <div className="cursor-pointer">
+            <IoChevronBack size={30} color={twTheme.colors.emerald[600]} />
+          </div>
+
+          <div className="border border-gray-200 p-3 rounded-md shadow-sm bg-green-50 ">
+            <Icon size={30} color={twTheme.colors.emerald[700]} />
+          </div>
+
         </div>
 
         <div className="flex flex-col gap-2 flex-1">
