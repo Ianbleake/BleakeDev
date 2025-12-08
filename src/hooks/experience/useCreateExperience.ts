e348b88d06ae48521a41a1797d01c4a3ca136c97
Ivan Rangel <159199996+IvanRangelKubo@users.noreply.github.com>
2025-12-08 11:13:28 -0600

    fix: create experience hook

--- src/hooks/experience/useCreateExperience.ts
+++ src/hooks/experience/useCreateExperience.ts
@@ -3,41 +3,46 @@ import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { createAchievement } from '@/services/achievements/createAchievement';
 import { useRouter } from "next/navigation";
+import { useExperienceStorage } from "@/storage/Admin/experienceStorage";
 
 export default function useCreateExperience() {
 
   const router = useRouter();
 
+  const { addExperience } = useExperienceStorage();
+
   const createExperienceMutation = useMutation({
     mutationKey: ['createExperience'],
-    mutationFn: async (newExperience: NewExperience) => {
+    mutationFn: async (newExperience: NewExperience): Promise<Experience> => {
 
       const { achievements, ...experienceData } = newExperience;
-
+    
       const createdExperience = await createExperience(experienceData);
-
-      if( achievements.length > 0 && createdExperience?.id ) {
-
+    
+      if (achievements.length > 0 && createdExperience ) {
+    
         const newAchievements = achievements.map((achievement: NewAchievement) => ({
           description: achievement.description,
-          element_id: createdExperience.id,
+          element_id: createdExperience?.id,
           type: 'experience',
         }));
-
+    
         const createdAchievements = await Promise.all(
-          newAchievements.map((achievement: AddAchievementPayload) => createAchievement(achievement))
+          newAchievements.map((achievement: AddAchievementPayload) =>
+            createAchievement(achievement)
+          )
         );
-
+    
         return { ...createdExperience, achievements: createdAchievements };
-
       }
-
-      return createdExperience;
-
+    
+      return createdExperience as Experience;
     },
-    onSuccess: () => {
+    
+    onSuccess: (newExperience: Experience) => {
+      addExperience(newExperience);
       toast.success("Experience created successfully");
-      router.push("/admin/education"); 
+      router.push("/admin/experience"); 
     },
     onError: () => {
       toast.error("Error creating experience");
